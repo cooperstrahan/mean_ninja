e46ce07adbd503d5b4afd1253791db50452b2e5d
cstra <cooperstrahan@gmail.com>
2019-03-06 12:18:21 -0800

    Ninja 2

--- ninja.js
+++ ninja.js
@@ -18,12 +18,38 @@ function Ninja(name){
         this.health += 10;
         return this;
     }
+
+    this.punch = function(ninja) {
+        if (ninja instanceof Ninja){
+            ninja.health -= 5;
+            console.log(this.name + " punched "+ ninja.name);
+            return this;
+        } else {
+            console.log("You cannot fight someone who is not a Ninja!")
+        }
+    }
+
+    this.kick = function(ninja) {
+        if (ninja instanceof Ninja){
+            ninja.health -= 15;
+            console.log(this.name + " kicked "+ ninja.name);
+            console.log(ninja)
+            return this;
+        } else {
+            console.log("You cannot fight someone who is not a Ninja!")
+        }
+    }
 }
 
 var ninja1 = new Ninja("Hyabusa");
+var ninja2 = new Ninja("Goku");
 ninja1.sayName();
+ninja2.sayName();
+
+ninja1.punch(ninja2);
+ninja2.kick(ninja1);
 // -> "My ninja name is Hyabusa!"
-ninja1.showStats();
-// -> "Name: Hayabusa, Health: 100, Speed: 3, Strength: 3"
-ninja1.drinkSake();
-ninja1.showStats();
+// ninja1.showStats();
+// // -> "Name: Hayabusa, Health: 100, Speed: 3, Strength: 3"
+// ninja1.drinkSake();
+// ninja1.showStats();
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+ninja1.showStats();
+// // -> "Name: Hayabusa, Health: 100, Speed: 3, Strength: 3"
+// ninja1.drinkSake();
+// ninja1.showStats();
